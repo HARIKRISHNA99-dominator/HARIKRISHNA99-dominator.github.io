7f46e714ee536e276ed53b53297cd0fc40481c2c
Hari Krishna P <72791901+HARIKRISHNA99-dominator@users.noreply.github.com>
2025-07-06 09:39:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,51 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
 
-    <script src="https://cdn.jsdelivr.net/npm/vanilla-tilt@1.7.0/dist/vanilla-tilt.min.js"></script>
-    <script>
-    VanillaTilt.init(document.querySelectorAll(".card"), {
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.navbar .menu li a').click(function () {
+    $('html').css("scrollBehavior", "smooth");
+  });
+
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
+
+  var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  VanillaTilt.init(document.querySelectorAll(".card"), {
     max: 15,
     speed: 400,
     glare: true,
     "max-glare": 0.4,
+  });
+
+  setTimeout(function () {
+    $('.jump-text').addClass('jump-disable');
+  }, 60000);
+
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow');
+  });
 });
-</script>
 
-
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
