f63c1adf146dec3f2cf1e7aba10f5bd502d52475
Hari Krishna P <72791901+HARIKRISHNA99-dominator@users.noreply.github.com>
2025-07-06 10:01:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,48 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $('.navbar').addClass("sticky");
-    } else {
-      $('.navbar').removeClass("sticky");
-    }
-    if (this.scrollY > 500) {
-      $('.scroll-up-btn').addClass("show");
-    } else {
-      $('.scroll-up-btn').removeClass("show");
-    }
-  });
+    // Sticky navbar and scroll-up button toggle
+    $(window).scroll(function () {
+        $('.navbar').toggleClass("sticky", this.scrollY > 20);
+        $('.scroll-up-btn').toggleClass("show", this.scrollY > 500);
+    });
 
-  $('.scroll-up-btn').click(function () {
-    $('html').animate({ scrollTop: 0 });
-    $('html').css("scrollBehavior", "auto");
-  });
+    // Scroll-up button click
+    $('.scroll-up-btn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        $('html').css("scrollBehavior", "auto");
+    });
 
-  $('.navbar .menu li a').click(function () {
-    $('html').css("scrollBehavior", "smooth");
-  });
+    // Smooth scroll on menu click
+    $('.navbar .menu li a').click(function () {
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-  });
+    // Toggle menu
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-  var typed = new Typed(".typing", {
-    strings: ["Data Analyst","Web Developer", "Developer", "Designer","Machine Learning Engineer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
+    // Typed.js animation
+    const options = {
+        strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    };
+    new Typed(".typing", options);
+    new Typed(".typing-2", options);
 
-  VanillaTilt.init(document.querySelectorAll(".card"), {
-    max: 15,
-    speed: 400,
-    glare: true,
-    "max-glare": 0.4,
-  });
-
-  setTimeout(() => {
-  document.querySelectorAll('.jump-text').forEach(el => {
-    el.classList.add('jump-disable');
-  });
-  }, 60000);
-
-  setTimeout(function () {
-    $('.jump-text').addClass('jump-disable');
-  }, 60000);
-
-  $(window).on('load', function () {
-    $('#preloader').fadeOut('slow');
-  });
+    // Owl carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: { items: 1, nav: false },
+            600: { items: 2, nav: false },
+            1000: { items: 3, nav: false }
+        }
+    });
 });
-
