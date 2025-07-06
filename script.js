307797edc9be1a9db68e6836c31d97c9e2b44737
Hari Krishna P <72791901+HARIKRISHNA99-dominator@users.noreply.github.com>
2025-07-06 10:08:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
-$(document).ready(function () {
-    // Sticky navbar and scroll-up button toggle
-    $(window).scroll(function () {
-        $('.navbar').toggleClass("sticky", this.scrollY > 20);
-        $('.scroll-up-btn').toggleClass("show", this.scrollY > 500);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
-    // Scroll-up button click
-    $('.scroll-up-btn').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    // Smooth scroll on menu click
-    $('.navbar .menu li a').click(function () {
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // Toggle menu
-    $('.menu-btn').click(function () {
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // Typed.js animation
-    const options = {
+    // typing text animation script
+    var typed = new Typed(".typing", {
         strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
-    };
-    new Typed(".typing", options);
-    new Typed(".typing-2", options);
+    });
 
-    // Owl carousel
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Developer", "Free Styler", "Designer", "Interior Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    setTimeout(function(){
+    $('.jump-text').addClass('jump-disable');
+    }, 60000); // stops jumping after 1 minute
+
+
+    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
-        autoplay: true,
-        autoplayTimeout: 2000,
+        autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0: { items: 1, nav: false },
-            600: { items: 2, nav: false },
-            1000: { items: 3, nav: false }
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
         }
     });
 });
